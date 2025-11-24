8853cb9fcf5369aabc027906164f9d8a696d408a
wildanfajar <wildanfajarpamungkas964@gmail.com>
2025-11-24 14:46:15 +0700

    menambah fitur kelvin dan mengubah warna saat diblok

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ function setupTemperatureConverter() {
   const celsiusInput = document.getElementById("celsius");
   const fahrenheitInput = document.getElementById("fahrenheit");
   const reamurInput = document.getElementById("reamur");
+  const kelvinInput = document.getElementById("kelvin");
 
   celsiusInput.addEventListener("input", function () {
     const celsiusValue = parseFloat(this.value) || 0;
     fahrenheitInput.value = ((celsiusValue * 9) / 5 + 32).toFixed(2);
     reamurInput.value = (celsiusValue * 0.8).toFixed(2);
+    kelvinInput.value = (celsiusValue + 273.15).toFixed(2);
   });
 
   fahrenheitInput.addEventListener("input", function () {
     const fahrenheitValue = parseFloat(this.value) || 0;
     celsiusInput.value = (((fahrenheitValue - 32) * 5) / 9).toFixed(2);
     reamurInput.value = (((fahrenheitValue - 32) * 4) / 9).toFixed(2);
+    kelvinInput.value = (((fahrenheitValue - 32) * 5) / 9 + 273.15).toFixed(2);
   });
   reamurInput.addEventListener("input", function () {
     const reamurValue = parseFloat(this.value) || 0;
     celsiusInput.value = (reamurValue * 1.25).toFixed(2);
     fahrenheitInput.value = (reamurValue * 2.25 + 32).toFixed(2);
+    kelvinInput.value = (reamurValue * 1.25 + 273.15).toFixed(2);
+  });
+  kelvinInput.addEventListener("input", function () {
+    const kelvinValue = parseFloat(this.value) || 0;
+    celsiusInput.value = (kelvinValue - 273.15).toFixed(2);
+    fahrenheitInput.value = (((kelvinValue - 273.15) * 9) / 5 + 32).toFixed(2);
+    reamurInput.value = ((kelvinValue - 273.15) * 0.8).toFixed(2);
   });
 }
 
@@ -30,6 +40,7 @@ if (resetButton) {
     document.getElementById("celsius").value = "";
     document.getElementById("fahrenheit").value = "";
     document.getElementById("reamur").value = "";
+    document.getElementById("kelvin").value = "";
   });
 } else {
   console.error("Element dengan id 'reset' tidak ditemukan");
